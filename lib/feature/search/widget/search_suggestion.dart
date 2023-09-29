e946a8fb53c88007f58543341f157e3c98ff1376
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2023-09-29 21:20:23 +0200

    feature: Use BannerTile component

--- lib/feature/search/widget/search_suggestion.dart
+++ lib/feature/search/widget/search_suggestion.dart
@@ -1,3 +1,4 @@
+import 'package:banner_listtile/banner_listtile.dart';
 import 'package:flutter/material.dart';
 import 'package:homer/core/book/domain/entity/book_entity.dart';
 import 'package:homer/core/book/domain/use_case/display_suggested_book/suggested_book_bloc.dart';
@@ -17,15 +18,40 @@ class SearchSuggestion extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-        leading: SizedBox(
-          height: 250,
-          width: 60,
-          child: Image.network(book.thumbnailAddress!, fit: BoxFit.cover),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        elevation: 3,
+        child: BannerListTile(
+          bannerTextColor: Colors.black,
+          bannerColor: Colors.amber,
+          bannerText: book.rating.toStringAsFixed(2),
+          onTap: () => _onTap(context),
+          backgroundColor: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          imageContainer: Image(
+            image: NetworkImage(book.thumbnailAddress!),
+            fit: BoxFit.cover,
+          ),
+          title: Text(
+            book.title,
+            style: const TextStyle(
+              fontSize: 17,
+              color: Colors.black,
+            ),
+            overflow: TextOverflow.ellipsis,
+            maxLines: 1,
+          ),
+          subtitle: Text(
+            'by ${book.author}',
+            style: const TextStyle(
+              fontSize: 13,
+              color: Colors.black,
+            ),
+          ),
         ),
-        onTap: () => _onTap(context),
-        title: Text(book.title),
-        subtitle: Text(book.subtitle));
+      ),
+    );
   }
 
   void _onTap(BuildContext context) {

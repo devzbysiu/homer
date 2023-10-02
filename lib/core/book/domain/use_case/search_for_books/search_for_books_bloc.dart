145f50c48951327980bad60cacf6e9d878cac5f4
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2023-10-02 17:40:59 +0200

    refactor: Code formatting

--- lib/core/book/domain/use_case/search_for_books/search_for_books_bloc.dart
+++ lib/core/book/domain/use_case/search_for_books/search_for_books_bloc.dart
@@ -3,15 +3,18 @@ import 'dart:async';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:homer/core/book/domain/entity/book_entity.dart';
 import 'package:homer/core/book/domain/repository/books_repository.dart';
+
 // ignore: depend_on_referenced_packages
 import 'package:meta/meta.dart';
 
 part 'search_for_books_event.dart';
+
 part 'search_for_books_state.dart';
 
 final class SearchForBooksBloc
     extends Bloc<SearchForBooksEvent, SearchForBooksState> {
-  SearchForBooksBloc({required this.booksRepo}) : super(SearchForBooksInitial()) {
+  SearchForBooksBloc({required this.booksRepo})
+      : super(SearchForBooksInitial()) {
     on<SearchInitiated>(_onSearchInitiated);
   }
 

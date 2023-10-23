351fd34cd8454e80894d01b62cf0db4d26be527f
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2023-10-23 19:14:40 +0200

    feature: Add homer backup on start

--- lib/main.dart
+++ lib/main.dart
@@ -41,9 +41,14 @@ void main() async {
 }
 
 Future<void> _prepareForBackup() async {
-  var content = await rootBundle.load('assets/backup.json');
   final directory = await getApplicationDocumentsDirectory();
-  var file = File('${directory.path}/backup.json');
-  file.writeAsBytesSync(content.buffer.asUint8List());
+  await _moveFile(directory, 'dante-backup.json');
+  await _moveFile(directory, 'homer-backup.json');
   return Future.value();
+}
+
+Future<void> _moveFile(Directory directory, String filename) async {
+  var content = await rootBundle.load('assets/$filename');
+  var danteBackup = File('${directory.path}/$filename');
+  danteBackup.writeAsBytesSync(content.buffer.asUint8List());
 }

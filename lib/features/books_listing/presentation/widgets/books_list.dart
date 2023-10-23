f84ec55956ad417a9a8b18146b50d20a745611da
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2023-10-23 18:29:30 +0200

    feature: Scroll to top on tab change

--- lib/features/books_listing/presentation/widgets/books_list.dart
+++ lib/features/books_listing/presentation/widgets/books_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:homer/features/navigation/presentation/bloc/app_tab_bloc.dart';
 import 'package:material_floating_search_bar_2/material_floating_search_bar_2.dart';
 
 import '../../../../core/utils/extensions/books_context_ext.dart';
@@ -8,23 +9,35 @@ import '../../../../core/utils/extensions/delete_books_context_ext.dart';
 import '../../../delete_book/presentation/bloc/delete_books_bloc.dart';
 import 'book_card.dart';
 
-final class BooksList extends StatelessWidget {
+final class BooksList extends StatefulWidget {
   const BooksList({super.key});
+
+  @override
+  State<BooksList> createState() => _BooksListState();
+}
+
+final class _BooksListState extends State<BooksList> {
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
     final books = context.booksOfCurrentTab();
-    if (books.isEmpty) {
-      return Container();
-    }
-    return BlocListener<DeleteBooksBloc, DeleteBooksState>(
-      listener: (context, state) {
-        if (state is BooksRemoved) context.refreshBooksList();
-      },
+    if (books.isEmpty) return Container();
+
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<DeleteBooksBloc, DeleteBooksState>(
+          listener: _refreshWhenBooksRemoved,
+        ),
+        BlocListener<AppTabBloc, AppTabState>(
+          listener: _jumpToTopOnTabChange,
+        )
+      ],
       child: GestureDetector(
         onTap: () => context.clearDeletionList(),
         child: FloatingSearchBarScrollNotifier(
           child: AlignedGridView.count(
+            controller: _scrollController,
             crossAxisCount: 2,
             cacheExtent: 0,
             itemCount: books.length,
@@ -33,5 +46,19 @@ final class BooksList extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _refreshWhenBooksRemoved(context, state) {
+    if (state is BooksRemoved) context.refreshBooksList();
+  }
+
+  void _jumpToTopOnTabChange(BuildContext _, AppTabState state) {
+    if (state is AppTabState) _scrollController.jumpTo(0);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _scrollController.dispose();
   }
 }

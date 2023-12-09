6e0233f737ea9dad04ed73fda49df531df74352f
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2023-12-09 20:15:20 +0100

    fix: Fix issue with service locator

--- lib/features/find_new_book/presentation/bloc/search/book_search_bloc.dart
+++ lib/features/find_new_book/presentation/bloc/search/book_search_bloc.dart
@@ -40,7 +40,7 @@ final class BookSearchBloc extends Bloc<BookSearchEvent, BookSearchState> {
 
   final ShareHandlerPlatform shareHandler;
 
-  final SearchForBooksImpl searchForBooks;
+  final SearchForBooks searchForBooks;
 
   final FetchSharedBook fetchSharedBook;
 

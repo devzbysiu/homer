2b5c90b77238e1cfb022b892c88992e1948a4c3b
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2024-01-09 16:26:15 +0100

    refactor: Cleanup app tab state

--- lib/features/navigation/presentation/bloc/app_tab_state.dart
+++ lib/features/navigation/presentation/bloc/app_tab_state.dart
@@ -2,17 +2,13 @@ part of 'app_tab_bloc.dart';
 
 @immutable
 final class AppTabState extends Equatable {
-  const AppTabState._({
-    this.currentTab = AppTab.readLater,
-  });
-
   final AppTab currentTab;
 
-  const AppTabState.readLater() : this._(currentTab: AppTab.readLater);
+  const AppTabState.readLater() : currentTab = AppTab.readLater;
 
-  const AppTabState.reading() : this._(currentTab: AppTab.reading);
+  const AppTabState.reading() : currentTab = AppTab.reading;
 
-  const AppTabState.read() : this._(currentTab: AppTab.read);
+  const AppTabState.read() : currentTab = AppTab.read;
 
   @override
   List<Object> get props => [currentTab];
